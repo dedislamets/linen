83e572d3adc3517c64bb5cb16fc3cee13fb3bac6
dedislamets <dedi.supatman@modena.co.id>
2021-10-21 10:29:44 +0700

    index laporan

--- application/views/laporan/index.php
+++ application/views/laporan/index.php
@@ -75,6 +75,8 @@
                       <select id="tahun_medis" name="tahun_medis" class="form-control" >
                         <option value="2021">2021</option>
                         <option value="2022">2022</option>
+                        <option value="2023">2023</option>
+                        <option value="2024">2024</option>
                       </select>
                     </div>
                   </div>
