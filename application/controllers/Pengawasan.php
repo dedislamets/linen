b8a70c10929b4071425ba277fca2a574a8e0ae8a
dedislamets <dedi.supatman@modena.co.id>
2021-12-01 00:23:24 +0700

    sdgfhg

--- application/controllers/Pengawasan.php
+++ application/controllers/Pengawasan.php
@@ -203,13 +203,7 @@ class Pengawasan extends CI_Controller {
 				      		}
 				      	}
 
-				      	$this->db->from("tb_soal_detail A");
-			      		$this->db->join("tb_inspeksi B","A.id=B.id_soal_detail");
-			      		$this->db->where("parent_id",$val['id']);
-			      		$this->db->where("tanggal",$tanggal);
-			      		$this->db->where("nilai > 0");
-			      		$sub_dinilai = $this->db->get()->result_array();
-			      		$data['soal'][$key]->count_sub_submit = count($sub_dinilai);
+				      	
 			      	}
 		      	}
 
@@ -266,6 +260,14 @@ class Pengawasan extends CI_Controller {
 			      			$total_flag--;
 			      		}
 			      	}
+
+			      	$this->db->from("tb_soal_detail A");
+		      		$this->db->join("tb_inspeksi B","A.id=B.id_soal_detail");
+		      		$this->db->where("parent_id",$val->id);
+		      		$this->db->where("tanggal",$tanggal);
+		      		$this->db->where("nilai > 0");
+		      		$sub_dinilai = $this->db->get()->result_array();
+		      		$data['pending'][$key]->count_sub_submit = count($sub_dinilai);
       			}
 
       			//cari soal yg ada sub nya
@@ -296,14 +298,8 @@ class Pengawasan extends CI_Controller {
 				      		$done_sub++;
 			      			$total_flag_sub--;
 				      	}
+
 				      	
-				      	$this->db->from("tb_soal_detail A");
-			      		$this->db->join("tb_inspeksi B","A.id=B.id_soal_detail");
-			      		$this->db->where("parent_id",$val['id']);
-			      		$this->db->where("tanggal",$tanggal);
-			      		$this->db->where("nilai > 0");
-			      		$sub_dinilai = $this->db->get()->result_array();
-			      		$data['pending'][$key]->count_sub_submit = count($sub_dinilai);
 			      	}
 		      	}
 
