99264837aa6e849cd28d52baab6a0a109c354b98
dedislamets <dedi.slamets@gmail.com>
2022-01-16 00:47:00 +0700

    sdfsdf

--- application/views/script/linenkeluar-create.php
+++ application/views/script/linenkeluar-create.php
@@ -378,7 +378,7 @@
     		finderr('Belum ada data linen di scan.!');
     	}else{
     		$.each(app.list_scan, function(index, obj) {
-    			if(obj.status != 'BERSIH' && obj.status != 'BARU' && obj.status != 'KIRIM'){
+    			if(obj.status != 'BERSIH' && obj.status != 'BARU' && obj.status == 'KIRIM'){
     				flag = false;
     				alert(obj.jenis + '-(' + obj.serial + ') tidak valid.!');
     				finderr(obj.jenis + '-(' + obj.serial + ') tidak valid.!');
